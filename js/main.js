dd5c71dd1784e6624b3d67df735534e292cd050e
serzmerz <serzmerz@gmail.com>
2017-10-22 16:33:07 +0300

    main page developed

---
+++ js/main.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+    // Инициализирует карусель
+    $(".start-slide").click(function(){
+        $("#myCarousel2").carousel('cycle');
+    });
+
+    // Осуществляет переход на предыдущий слайд
+    $(".prev-slide").click(function(){
+        $("#myCarousel2").carousel('prev');
+    });
+    // Осуществляет переход на следующий слайд
+    $(".next-slide").click(function(){
+        $("#myCarousel2").carousel('next');
+    });
+
+    $(".start-slide1").click(function(){
+        $("#myCarousel1").carousel('cycle');
+    });
+
+    // Осуществляет переход на предыдущий слайд
+    $(".prev-slide1").click(function(){
+        $("#myCarousel1").carousel('prev');
+    });
+    // Осуществляет переход на следующий слайд
+    $(".next-slide1").click(function(){
+        $("#myCarousel1").carousel('next');
+    });
+    $('#accordion').on('shown.bs.collapse', function (e) {
+        var elem = e.target.previousElementSibling.firstElementChild.lastElementChild.firstElementChild;
+        elem.className = 'flaticon-arrows';
+        var panelHeading = e.target.previousElementSibling;
+        panelHeading.classList.add('active');
+    });
+    $('#accordion').on('hidden.bs.collapse', function (e) {
+        var elem = e.target.previousElementSibling.firstElementChild.lastElementChild.firstElementChild;
+        elem.className = 'flaticon-arrows-1';
+        var panelHeading = e.target.previousElementSibling;
+        panelHeading.classList.remove('active');
+    });
+
+    $(".toggle_mnu").click(function() {
+        $(".sandwich").toggleClass("active");
+        $(".links").toggleClass("hidden");
+    });
+    $(".links a").click(function () {
+        $(".sandwich").removeClass("active");
+        $(".links").addClass("hidden");
+        }
+    );
+    jQuery("a.scrollto").click(function () {
+        elementClick = jQuery(this).attr("href")
+        destination = jQuery(elementClick).offset().top;
+        jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1100);
+        return false;
+    });
+});
