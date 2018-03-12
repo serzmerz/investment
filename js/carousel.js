4e75896bb27bf6555a1cb6f158611ab41ec9dc2b
serzmerz <serzmerz@gmail.com>
2018-03-12 23:05:29 +0200

    fix bugs, add carousel

---
+++ js/carousel.js
@@ -0,0 +1,34 @@
+(function($) {
+    $(function() {
+        var jcarousel = $('.jcarousel');
+
+        jcarousel
+            .on('jcarousel:reload jcarousel:create', function () {
+                var carousel = $(this),
+                    width = carousel.innerWidth();
+
+                if (width >= 993) {
+                    width = width / 4;
+                } else if (width >= 769) {
+                    width = width / 3;
+                } else if (width >= 450) {
+                    width = width / 2;
+                }
+
+                carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
+            })
+            .jcarousel({
+                wrap: 'circular'
+            });
+
+        $('.jcarousel-control-prev')
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .jcarouselControl({
+                target: '+=1'
+            });
+    });
+})(jQuery);
